b2bababb82615cb0127120e3460e3b9fae37f9ef
202010887유채연 <63452346+Yuchaeyeon@users.noreply.github.com>
2021-11-09 23:37:40 +0900

    Add files via upload
    hw5

---
+++ Homework5.java
@@ -0,0 +1,31 @@
+public class Homework5 {
+    private String CPU;
+    private String Memory;
+    private String HDD;
+    private String Color;
+    private String power;
+    private String Size;
+
+    public PersonalComputer{
+
+
+    }
+
+    public monitor{
+        System.out.println("Turning on the monitor.\n");
+        System.out.println("the spec of the monitor.\n");
+        String Size = 24 inch;
+        String power = 45W;
+        String Color = Black;
+    }
+
+    public Computer{
+        System.out.println("Turning on the computer.");
+        System.out.println("the spec of the computer.\n");
+        String CPU = Core i7;
+        String Memory = 16GB;
+        String HDD = 2TB;
+        String power = 500W;
+        String Color = White;
+    }
+}
