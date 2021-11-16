e707073878b73926aecd73252317f1ad715f6bf3
202010887유채연 <63452346+Yuchaeyeon@users.noreply.github.com>
2021-11-16 23:32:42 +0900

    Add files via upload

---
+++ Homework6.java
@@ -0,0 +1,15 @@
+public class Homework6 {
+    private Code;
+    private Name;
+    private Type;
+    
+    public static void main(String[] args){
+        OfflineCourse course1 = new OfflineCourse("HAEA9201", "Object Oriented Programming");
+        OnlineCourse course2 = new OnlineCourse("HAFL0012", "C Programming 1");
+        System.out.println (course1);
+        System.out.println (course2);
+
+    }
+
+
+}
